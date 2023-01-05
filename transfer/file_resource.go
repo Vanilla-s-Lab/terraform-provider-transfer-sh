c00de800c82675ba7559c41b850987953fb74d9e
Vanilla <osu_Vanilla@126.com>
2023-01-05 17:13:31 +0800

    feat[transfer/file_resource.go]: Simple handle upload.

--- transfer/file_resource.go
+++ transfer/file_resource.go
@@ -4,6 +4,14 @@ import (
 	"context"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 
+	"crypto/md5"
+	"encoding/hex"
+	"errors"
+	"io"
+	"net/http"
+	"os"
+	"path"
+
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 )
@@ -34,13 +42,52 @@ func (f fileResource) Schema(_ context.Context, _ resource.SchemaRequest, resp *
 			"file_path": schema.StringAttribute{Required: true},
 			"file_hash": schema.StringAttribute{Computed: true},
 
-			//https://github.com/dutchcoders/transfer.sh
+			// https://github.com/dutchcoders/transfer.sh
 			"link": schema.StringAttribute{Computed: true},
 		},
 	}
 }
 
-func (f fileResource) Create(context.Context, resource.CreateRequest, *resource.CreateResponse) {
+const URL = "https://transfer.sh"
+
+func (f fileResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var plan fileResource
+	req.Plan.Get(ctx, &plan)
+
+	filePath := plan.Path.ValueString()
+
+	// https://stackoverflow.com/questions/12518876/how-to-check-if-a-file-exists-in-go
+	if _, err := os.Stat(filePath); errors.Is(err, os.ErrNotExist) {
+		resp.Diagnostics.AddError("File not found", filePath)
+		return
+	}
+
+	fileName := path.Base(filePath)
+	url := URL + "/" + fileName
+
+	buf, _ := os.Open(filePath)
+
+	// https://pkg.go.dev/crypto/md5#example-New-File
+	h := md5.New()
+	_, _ = io.Copy(h, buf)
+
+	//https://stackoverflow.com/questions/2377881/how-to-get-a-md5-hash-from-a-string-in-golang
+	hash := h.Sum(nil)
+	hashString := hex.EncodeToString(hash[:])
+
+	plan.Hash = types.StringValue(hashString)
+
+	_, _ = buf.Seek(0, 0)
+
+	// https://pkg.go.dev/net/http
+
+	request, _ := http.NewRequest(http.MethodPut, url, buf)
+	response, _ := (&http.Client{}).Do(request)
+
+	body, _ := io.ReadAll(response.Body)
+	plan.Link = types.StringValue(string(body))
+
+	resp.State.Set(ctx, plan)
 }
 
 func (f fileResource) Read(context.Context, resource.ReadRequest, *resource.ReadResponse) {
