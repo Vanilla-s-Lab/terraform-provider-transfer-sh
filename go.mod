54a483ce76ac3baa14fc02b0a50994d326d18509
Vanilla <osu_Vanilla@126.com>
2023-01-05 12:43:03 +0800

    rename[go.mod, main.go]: transfer.sh -> transfer-sh.

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module terraform-provider-transfer.sh
+module terraform-provider-transfer-sh
 
 go 1.19
 
--- main.go
+++ main.go
@@ -4,12 +4,12 @@ import (
 	"context"
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 
-	"terraform-provider-transfer.sh/transfer"
+	"terraform-provider-transfer-sh/transfer"
 )
 
 func main() {
 	//goland:noinspection GoUnhandledErrorResult
 	providerserver.Serve(context.Background(), transfer.New, providerserver.ServeOpts{
-		Address: "hashicorp.com/edu/transfer.sh",
+		Address: "hashicorp.com/edu/transfer-sh",
 	})
 }
